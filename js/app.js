6eae5560640780e2ba0f411a4f89b320f50db775
NanoCare-CarCare <a.haneen655@outlook.com>
2026-08-08 00:05:56 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -218,4 +218,57 @@ function translateStatus(status) {
 
 }
 
+const documentsDiv = document.getElementById("documents");
+const maintenanceDiv = document.getElementById("maintenance");
+
+documentsDiv.innerHTML = "";
+maintenanceDiv.innerHTML = "";
+
+documents.forEach(doc => {
+
+    // المستندات والشهادات
+    if (doc.document_type === "document" ||
+        doc.document_type === "certificate") {
+
+        documentsDiv.innerHTML += `
+        <div class="service">
+
+            <span>${doc.name}</span>
+
+            <a href="${doc.file_url}"
+               target="_blank"
+               class="status finished">
+
+               عرض
+
+            </a>
+
+        </div>
+        `;
+    }
+
+    // جدول الصيانة
+    else if (doc.document_type === "maintenance") {
+
+        maintenanceDiv.innerHTML += `
+
+        <div class="service">
+
+            <span>${doc.name}</span>
+
+            <a href="${doc.file_url}"
+               target="_blank"
+               class="status accepted">
+
+               فتح
+
+            </a>
+
+        </div>
+
+        `;
+    }
+
+});
+
 loadPassport();
